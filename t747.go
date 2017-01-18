da1915e9edebe208a1b1bc7de4cfd597ddbf8abc
kakuilan <kakuilan@163.com>
2017-01-18 12:51:17 +0800

    自定义切片类型

---
+++ t747.go
@@ -0,0 +1,25 @@
+//自定义切片类型
+package main
+import "fmt"
+
+type Product struct {
+    name string
+    price float64
+}
+
+func (product Product) String() string{
+    return fmt.Sprintf("%s (%.2f)", product.name, product.price)
+}
+
+func main() {
+    products := []*Product {{"Spanner", 3.99}, {"Wrench", 2.49}, {"Screwdriver", 1.99}}
+    fmt.Println(products)
+
+    for _,product := range products{
+        product.price += 0.5
+    }
+    fmt.Println(products)
+
+
+
+}
