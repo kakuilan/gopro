49e051ffbb2cfe5004bf954a232e0a4cb6f2593c
kakuilan <kakuilan@163.com>
2017-05-24 12:58:45 +0800

    恢复异常,将异常转为错误返回

--- t774.go
+++ t774.go
@@ -9,11 +9,14 @@ func ConvertInt64ToInt(x int64) int {
     if math.MinInt32 <=x && x <=math.MaxInt32 {
         return int(x)
     }
+    //抛出异常
     panic(fmt.Sprintf("%d is out of the int32 range", x))
 }
 
 func IntFromInt64(x int64) (i int,err error) {
+    //延迟处理
     defer func() {
+        //恢复异常,转为一个错误返回
         if e := recover(); e!=nil {
             err = fmt.Errorf("%v", e)
         }
