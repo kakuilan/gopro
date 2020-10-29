d8afcd5da79f0052c27c502d1c259d9b790c6133
kakuilan <kakuilan@163.com>
2020-10-29 13:03:08 +0800

    sync.Mutex互斥锁和sync.RWMutex读写锁的性能比较

---
+++ t0024.go
@@ -0,0 +1,52 @@
+//sync.Mutex互斥锁和sync.RWMutex读写锁的性能比较
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	var (
+		mux    sync.Mutex
+		state1 = map[string]int{
+			"a": 65,
+		}
+		muxTotal uint64
+
+		rw     sync.RWMutex
+		state2 = map[string]int{
+			"a": 65,
+		}
+		rwTotal uint64
+	)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				mux.Lock()
+				_ = state1["a"]
+				mux.Unlock()
+				atomic.AddUint64(&muxTotal, 1)
+			}
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				rw.RLock()
+				_ = state2["a"]
+				rw.RUnlock()
+				atomic.AddUint64(&rwTotal, 1)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("sync.Mute readOps is", muxTotal)
+	fmt.Println("sync.RWMutex readOps is", rwTotal)
+
+}
