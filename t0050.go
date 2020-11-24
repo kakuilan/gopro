e2b4b7df2b829a9f9f602a997bd97975aabdf369
kakuilan <kakuilan@163.com>
2020-11-24 13:11:59 +0800

    cgo例子,引用C库

---
+++ t0050.go
@@ -0,0 +1,17 @@
+//cgo例子,引用C库
+package main
+
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func Print(s string) {
+	cs := C.CString(s)
+	defer C.free(unsafe.Pointer(cs))
+	C.fputs(cs, (*C.FILE)(C.stdout))
+}
+
+func main() {
+	Print("Hello world\n")
+}
