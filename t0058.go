1b862b6c828f5b39533321bc95cad770db20b70b
kakuilan <kakuilan@163.com>
2020-12-09 13:13:28 +0800

    日期和时间

---
+++ t0058.go
@@ -0,0 +1,34 @@
+//日期和时间
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var week time.Duration
+
+func main() {
+	t := time.Now()
+
+	fmt.Println(t)
+	fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year())
+
+	t = time.Now().UTC()
+	fmt.Println(t)
+	fmt.Println(time.Now())
+
+	//纳秒
+	week = 60 * 60 * 24 * 7 * 1e9
+	week_from_now := t.Add(week)
+	fmt.Println(week_from_now)
+
+	//格式化
+	fmt.Println(t.Format(time.RFC822))
+	fmt.Println(t.Format(time.ANSIC))
+	fmt.Println(t.Format("02 Jan 2006 15:04"))
+
+	s := t.Format("20060102")
+	fmt.Println(t, "=>", s)
+
+}
