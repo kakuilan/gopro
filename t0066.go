36cf77116963b298e222f058ee22dda6c44795c8
kakuilan <kakuilan@163.com>
2020-12-19 12:52:34 +0800

    传递变长参数

---
+++ t0066.go
@@ -0,0 +1,27 @@
+//传递变长参数
+package main
+
+import "fmt"
+
+func main() {
+	x := min(1, 3, 2, 0)
+	fmt.Printf("The minimum is: %d\n", x)
+
+	arr := []int{7, 9, 3, 5, 2}
+	x = min(arr...)
+	fmt.Printf("The minimum is: %d\n", x)
+}
+
+func min(a ...int) int {
+	if len(a) == 0 {
+		return 0
+	}
+
+	min := a[0]
+	for _, v := range a {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
