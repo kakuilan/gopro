b0db829e6664243931b2d9bd3fa3043e36a7290f
kakuilan <kakuilan@163.com>
2020-12-24 13:04:56 +0800

    斐波那契使用缓存提升计算

---
+++ t0070.go
@@ -0,0 +1,38 @@
+//斐波那契使用缓存提升计算
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const LIM = 41
+
+var fibs [LIM]uint64
+
+func fibonacci(n int) (res uint64) {
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci(n-1) + fibonacci(n-2)
+	}
+	fibs[n] = res
+	return
+}
+
+func main() {
+	var result uint64 = 0
+	start := time.Now()
+	for i := 0; i < LIM; i++ {
+		result = fibonacci(i)
+		fmt.Printf("fibonacci(%d) is: %d\n", i, result)
+	}
+	end := time.Now()
+	delta := end.Sub(start)
+	fmt.Printf("fibonacci took this amout of time: %s\n", delta)
+
+}
