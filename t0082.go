0cbcf8d457ad810207e30c93449b78528d2312af
kakuilan <kakuilan@163.com>
2021-01-19 13:00:35 +0800

    fix 结构体

--- t0082.go
+++ t0082.go
@@ -15,6 +15,14 @@ func main() {
 	ms.f1 = 15.5
 	ms.str = "Chris"
 
+	//初始化结构体
+	ms1 := &struct1{8, 3.14, "Zhang3"}
+	//或者
+	var ms2 struct1
+	ms2 = struct1{99, 2.899, "Li4"}
+
 	fmt.Printf("struct1: %v\n", ms)
+	fmt.Println(ms1)
+	fmt.Println(ms2)
 
 }
