125be9628cea51bfb6d89cee7346d2b18d171ff3
kakuilan <kakuilan@163.com>
2022-02-15 14:57:18 +0800

    鸭子类型

---
+++ t0105.go
@@ -0,0 +1,33 @@
+//鸭子类型
+package main
+
+import "fmt"
+
+//定义鸭子接口
+type IDuck interface {
+	Quack()
+	Walk()
+}
+
+func DuckDance(duck IDuck) {
+	for i := 1; i <= 3; i++ {
+		duck.Quack()
+		duck.Walk()
+	}
+}
+
+type Bird struct {
+}
+
+func (b *Bird) Quack() {
+	fmt.Println("I am quacking!")
+}
+
+func (b *Bird) Walk() {
+	fmt.Println("I am walking!")
+}
+
+func main() {
+	b := new(Bird)
+	DuckDance(b)
+}
