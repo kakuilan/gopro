87418a31b4e65926d122faa402a56702ff0f738e
kakuilan <kakuilan@163.com>
2022-04-12 09:21:36 +0800

    抛出panic

---
+++ t0118.go
@@ -0,0 +1,11 @@
+//panic
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Starting the program")
+	panic("A server error occurred: stopping the program!")
+	fmt.Println("Endding the program")
+
+}
